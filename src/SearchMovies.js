4be7145727ec38ee00c8663b71f87a9b62f92975
dek-haji <deeeqhaji@gmail.com>
2020-06-21 16:28:45 -0500

    fetching API from the external data

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+console.log(`${process.env.REACT_APP_MOVIES_API}`);
 function SearchMovies() {
     const SearchMovies = async (e) => {
         e.preventDefault();
-        console.log("submit")
+        console.log("submit");
+        const query = "Jurassic Park"
+        const API_KEY = process.env.REACT_APP_MOVIES_API;
+        console.log(API_KEY)
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data)
+        } catch (err) {
+            console.log(err)
+        }
     }
     return (
     <form className="form" onSubmit={SearchMovies}>
