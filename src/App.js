b062a54c0433f8f6b052d3aeaf58be1c73a30bb4
dek-haji <deeeqhaji@gmail.com>
2020-06-21 15:22:34 -0500

    styled the movie search component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="container">
         <h1 className="title">Movie Search</h1>
-      <SearchMovies />
+          <SearchMovies />
     </div>
   );
 }
--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -4,7 +4,10 @@ function SearchMovies() {
     return (
     <form className="form">
         <label className="label" htmlFor="query" >Movie Name</label>
-        <input className="input" type="text" placeholder="movies" name="query"></input>
+            <input className="input"
+                type="text"
+                placeholder="Spider Mans"
+                name="query"></input>
         <button className="button" type="submit">Search</button>
         </form>
     )
